a671a736dc2259aa58ffe697f181bb3e20796d86
stonefullstm <stonefull.stm@gmail.com>
2023-01-23 11:15:12 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='page-body'>
+      <img
+        className='background'
+        src='chess-background.jpg'
+        alt=""
+      />
+      <div class="page-content">
+        <header>
+          <p className='page-title'>Chess Clock</p>
+        </header>
+        <p>White</p>
+        <p>Black</p>
+        </div>
+    </div>
+  );
+}
+
+export default App;
